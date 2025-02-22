dde24db6b83205adc2aa6438290c8727df10b7a4
gpimenta07 <gabrielcarvalhopimenta@gmail.com>
2025-02-22 15:08:55 -0300

    Melhorias...

---
+++ script.js
@@ -0,0 +1,72 @@
+function removeClassHtml() {
+    document.getElementById("newHtml").classList.remove("hidden");
+  }
+
+  function putClassHtml() {
+    document.getElementById("newHtml").classList.add("hidden");
+  }
+
+  function removeClassCss() {
+    document.getElementById("newCss").classList.remove("hidden");
+  }
+
+  function putClassCss() {
+    document.getElementById("newCss").classList.add("hidden");
+  }
+
+  function removeClassJs() {
+    document.getElementById("newJs").classList.remove("hidden");
+  }
+
+  function putClassJs() {
+    document.getElementById("newJs").classList.add("hidden");
+  }
+
+  function removeClassRjs() {
+    document.getElementById("newRjs").classList.remove("hidden");
+  }
+
+  function putClassRjs() {
+    document.getElementById("newRjs").classList.add("hidden");
+  }
+
+  function removeClassTcss() {
+    document.getElementById("newTcss").classList.remove("hidden");
+  }
+
+  function putClassTcss() {
+    document.getElementById("newTcss").classList.add("hidden");
+  }
+
+  function removeClassGit() {
+    document.getElementById("newGit").classList.remove("hidden");
+  }
+
+  function putClassGit() {
+    document.getElementById("newGit").classList.add("hidden");
+  }
+
+  function removeClassGh() {
+    document.getElementById("newGh").classList.remove("hidden");
+  }
+
+  function putClassGh() {
+    document.getElementById("newGh").classList.add("hidden");
+  }
+
+  function removeClassGh() {
+    document.getElementById("newGh").classList.remove("hidden");
+  }
+
+  function putClassGh() {
+    document.getElementById("newGh").classList.add("hidden");
+  }
+
+  function removeClassProjectCalc() {
+    document.getElementById("project-calc").classList.remove("hidden");
+  }
+
+  function putClassProjectCalc() {
+    document.getElementById("project-calc").classList.add("hidden");
+  }
+
